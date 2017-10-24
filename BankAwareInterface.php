0199b55b7bd8bb05aaa762826e9d76f70b8c3f88
Anonymous <null@void>
2017-10-25 02:16:17 +0500

    BankAwareInterface

---
+++ BankAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Validator/CreditCard may implement this interface if it is capable to query BIN.
+ */
+interface BankAwareInterface
+{
+    protected $banks = [
+        59 => '/alfa/i',
+        77 => '/sber/i',
+        71 => '/Tinkoff/i'
+    ];
+    
+    /**
+     * Method query use NeutrinoAPI and return parsed response content.
+     * If bank name match a regex in $banks, add 'bankcode' field with $banks key to response.
+     * 
+     * @param string $cardNumber
+     * @throws ApiError If API query end up with error. $message is API error message.
+     * @return array
+     */
+    public function getBank($cardNumber);
+}
